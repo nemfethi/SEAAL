2e31cb89ea00163b981ed6dce9bd1af52cf69d07
fethi Nemir <feti.Nemir@gmail.com>
2019-05-21 08:22:57 +0100

    Corrections montants

--- Reprint/insAlg_ImpFac.sql
+++ Reprint/insAlg_ImpFac.sql
@@ -14,3 +14,10 @@ select idtclt, column6 as Ftype, count(*) from alg_impfac
 group by idtclt, column6
 ) group by idtclt
 having count(*)=1);
+
+/* pour les tests*/
+insert into tche (IDTTYPTCHE, IDTAGT, STT, DATDMD, DATRLS, MODDCL, ADRIMP, PRM1, PRM2) 
+select 'TRT8005', 'M23361', '', sysdate, to_date(null), 'R', 'LPT1', IdtFac , 'D' from
+alg_impfac, fac
+where rfr=numfct
+and Alg_impfac.IdtClt in (125892, 80315, 511894, 462590 ,120358,261260, 112309,336487,181987) 
